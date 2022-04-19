86a6e3f957decdbb93d2b0104993583c7758df88
mumja92 <mumja92@gmail.com>
2022-04-19 12:11:05 +0200

    Hide elo in some places
    Elo is hidden, but sometimes it needs to be reloaded

--- hide_elo.js
+++ hide_elo.js
@@ -1,1 +1,11 @@
-document.body.style.border = "5px solid red";
+// Elo showed next to nicknames
+var userTaglinePopoverRatingArray = document.getElementsByClassName("user-tagline-popover-rating");
+for (var i=0; i<userTaglinePopoverRatingArray.length; i++){
+    userTaglinePopoverRatingArray[i].innerHTML='';
+}
+
+// User highest rating
+var statTextDataHighestArray = document.getElementsByClassName("stat-text-data-highest");
+for (var i=0; i<statTextDataHighestArray.length; i++){
+    statTextDataHighestArray[i].innerHTML='-';
+}
