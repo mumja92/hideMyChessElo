377943468bf2d46ffb46460d4cb9eebcaa7cb578
mumja92 <mumja92@gmail.com>
2022-04-19 17:42:41 +0200

    Remove elo from post game screen

--- hide_elo.js
+++ hide_elo.js
@@ -1,41 +1,60 @@
+const eloReplacerString = "(?)"
+
 function hideEloNextToNicknames(){
     var userTaglinePopoverRatingArray = document.getElementsByClassName("user-tagline-popover-rating");
     for (var i=0; i<userTaglinePopoverRatingArray.length; i++){
-        userTaglinePopoverRatingArray[i].innerHTML='-';
+        userTaglinePopoverRatingArray[i].innerHTML=eloReplacerString;
     }
 }
 
+function hideEloNextToNicknamesInGame(){
+    var elementArray = document.getElementsByClassName("user-tagline-rating");
+    for (var i=0; i<elementArray.length; i++){
+        elementArray[i].innerHTML=eloReplacerString;
+    }
+}
 
 function hideHighestElo(){
     var statTextDataHighestArray = document.getElementsByClassName("stat-text-data-highest");
     for (var i=0; i<statTextDataHighestArray.length; i++){
-        statTextDataHighestArray[i].innerHTML='-';
+        statTextDataHighestArray[i].innerHTML=eloReplacerString;
     }
+}
+
+function hideAfterGameElo(){
+    document.getElementsByClassName("game-over-rating-component")[0].remove();
 }
 
 function run(){
     if (document.URL.includes("chess.com/home")){
-        if (document.getElementsByClassName("user-tagline-popover-rating").length > 1){
+        if (document.getElementsByClassName("user-tagline-popover-rating").length > 0){
             hideEloNextToNicknames();
             hideHighestElo();
         }
         else{
-            setTimeout(() => {
-                run();
-            }, 100);
+            setTimeout(run, 100);
         }
         return
     }
 
     if (document.URL.includes("chess.com/member")){
-        if (document.getElementsByClassName("user-tagline-popover-rating").length > 1){
+        if (document.getElementsByClassName("user-tagline-popover-rating").length > 0){
             hideEloNextToNicknames();
             hideHighestElo();
         }
         else{
-            setTimeout(() => {
-                run();
-            }, 100);
+            setTimeout(run, 100);
+        }
+        return
+    }
+
+    if (document.URL.includes("chess.com/game/live")){
+        if (document.getElementsByClassName("game-over-rating-component").length > 0){
+            hideAfterGameElo();
+            hideEloNextToNicknamesInGame();
+        }
+        else{
+            setTimeout(run, 100);
         }
         return
     }
