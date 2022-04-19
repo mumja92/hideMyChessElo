2fd438abb2201376cca7b6310fc9f44ff8f5be5d
mumja92 <mumja92@gmail.com>
2022-04-19 20:45:00 +0200

    Hide elo for 'archive' subpage. Introduce generic 'hide' function
    All existing 'hide' funcitons are merged into one.
    Elo is now hidden on archive subpage.

--- hide_elo.js
+++ hide_elo.js
@@ -1,43 +1,18 @@
 const eloReplacerString = "(?)"
 const refreshInterval = 50
 
-function clearEloFromElementArray(elementArray){
-    for (var i=0; i<elementArray.length; i++){
-        elementArray[i].innerHTML=eloReplacerString;
-    }
-}
-
-function hideEloNextToNicknames(infinite = false){
-    var elementArray = document.getElementsByClassName("user-tagline-popover-rating");
-    if (elementArray.length > 0){
-        clearEloFromElementArray(elementArray);
+function hideEloByClassTag(tag, invalidCount=0, infinite=false){
+    var elementArray = document.getElementsByClassName(tag);
+    if (elementArray.length > invalidCount){
+        for (var i=0; i<elementArray.length; i++){
+            elementArray[i].innerHTML=eloReplacerString;
+        }
         if (infinite){
-            setTimeout(function(){hideEloNextToNicknames(infinite)}, refreshInterval);
+            setTimeout(function(){hideEloByClassTag(tag, invalidCount, infinite)}, refreshInterval);
         }
     }
     else{
-        setTimeout(function(){hideEloNextToNicknames(infinite)}, refreshInterval);
-    }
-}
-
-function hideEloNextToNicknamesInGame(){
-    var elementArray = document.getElementsByClassName("user-tagline-rating");
-    // it is not initialized when length == 1
-    if (elementArray.length > 1){
-        clearEloFromElementArray(elementArray);
-    }
-    else{
-        setTimeout(hideEloNextToNicknamesInGame, refreshInterval);
-    }
-}
-
-function hideHighestElo(){
-    var elementArray = document.getElementsByClassName("stat-text-data-highest");
-    if (elementArray.length > 0){
-        clearEloFromElementArray(elementArray);
-    }
-    else{
-        setTimeout(hideHighestElo, refreshInterval);
+        setTimeout(function(){hideEloByClassTag(tag, invalidCount, infinite)}, refreshInterval);
     }
 }
 
@@ -49,25 +24,28 @@ function hideAfterGameElo(){
     else{
         setTimeout(hideAfterGameElo, refreshInterval);
     }
-
 }
 
 function run(){
     if (document.URL.includes("chess.com/home")){
-        hideEloNextToNicknames();
-        hideHighestElo();
+        hideEloByClassTag("user-tagline-popover-rating");
+        hideEloByClassTag("stat-text-data-highest");
         return
     }
 
     if (document.URL.includes("chess.com/member")){
-        hideEloNextToNicknames(true);
-        hideHighestElo();
+        hideEloByClassTag("user-tagline-popover-rating", invalidCount=0, infinite=true);
+        hideEloByClassTag("stat-text-data-highest");
         return
     }
 
     if (document.URL.includes("chess.com/game/live")){
-        hideEloNextToNicknamesInGame();
+        hideEloByClassTag("user-tagline-rating", invalidCount=1);
         hideAfterGameElo();
+        return
+    }
+    if (document.URL.includes("chess.com/games/archive")){
+        hideEloByClassTag("user-tagline-rating");
         return
     }
 }
