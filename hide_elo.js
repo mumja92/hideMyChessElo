fe42e0e4aa1104c838b094fbb492f209ec468ebc
mumja92 <mumja92@gmail.com>
2022-04-19 15:45:58 +0200

    Refactor to use timer
    Page data is dynamically created, so extension executes before data
    is loaded - it doesn't work. Using delay makes extension work.

--- hide_elo.js
+++ hide_elo.js
@@ -1,11 +1,23 @@
-// Elo showed next to nicknames
-var userTaglinePopoverRatingArray = document.getElementsByClassName("user-tagline-popover-rating");
-for (var i=0; i<userTaglinePopoverRatingArray.length; i++){
-    userTaglinePopoverRatingArray[i].innerHTML='';
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// User highest rating
-var statTextDataHighestArray = document.getElementsByClassName("stat-text-data-highest");
-for (var i=0; i<statTextDataHighestArray.length; i++){
-    statTextDataHighestArray[i].innerHTML='-';
+function hideEloNextToNicknames(){
+    var userTaglinePopoverRatingArray = document.getElementsByClassName("user-tagline-popover-rating");
+    for (var i=0; i<userTaglinePopoverRatingArray.length; i++){
+        userTaglinePopoverRatingArray[i].innerHTML='-';
+    }
 }
+
+
+function hideHighestElo(){
+    var statTextDataHighestArray = document.getElementsByClassName("stat-text-data-highest");
+    for (var i=0; i<statTextDataHighestArray.length; i++){
+        statTextDataHighestArray[i].innerHTML='-';
+    }
+}
+
+setTimeout(() => {
+    hideEloNextToNicknames();
+    hideHighestElo();
+}, 1000);
